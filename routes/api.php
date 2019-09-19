776ea5d51a8b035adfd74b16122ee1923c95c01b
Youssef Tharwat <yousseftharwat97@gmail.com>
2019-09-19 18:59:20 +0800

    feedbacking fix 10

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::get('users/{id}', 'UserController@show');
-Route::get('users/agent/feedback', 'UserController@agentFeedback');
+Route::post('users/agent/feedback', 'UserController@agentFeedback');
 
 Route::prefix('admin')->group(function () {
 
